5049abf98da54e788c5ac77c0e177f8140b069ea
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2023-12-23 22:41:10 +0000

    Fully working (but not optimized)

--- Projeto 3/projeto3.py
+++ Projeto 3/projeto3.py
@@ -14,43 +14,27 @@ for i in range(t):
 for i in range(p):
     bundles.append([int(i) for i in input().split()])
 
-# Cria variáveis de decisão
+# Cria variáveis de decisão: brinquedos e pacotes
 for i in range(t):
-    temp = LpVariable("x" + str(i + 1), 0, toys[i][1], cat=LpInteger)
+    temp = LpVariable("x" + str(i + 1), 0, None, cat=LpInteger)
     var_toys.append(temp)
-
-# Cria pacotes
 for i in range(p):
-    temp = LpVariable("y" + str(i + 1), 0, bundles[i][-1], cat=LpInteger)
+    temp = LpVariable("y" + str(i + 1), 0, None, cat=LpInteger)
     var_bundles.append(temp)
 
 # Função Objetivo
 prob += lpSum(var_toys[i] * toys[i][0] for i in range(t)) + lpSum(var_bundles[i] * bundles[i][-1] for i in range(p))
 
-# Restrições das variáveis de decisão
+# Restrição dos brinquedos e pacotes
 for i in range(t):
-    prob += var_toys[i] <= toys[i][1]
-
-'''
-print(toys)
-print(bundles)
-print(var_toys)
-print(var_bundles)
-'''
-
-# Restrição dos pacotes
-for i in range(p):
-    prob += var_toys[bundles[i][0] - 1] + var_bundles[i] <= toys[bundles[i][0] - 1][1]
-    prob += var_toys[bundles[i][1] - 1] + var_bundles[i] <= toys[bundles[i][1] - 1][1]
-    prob += var_toys[bundles[i][2] - 1] + var_bundles[i] <= toys[bundles[i][2] - 1][1]
-    prob += var_bundles[i] <= var_toys[bundles[i][0] - 1]
-    prob += var_bundles[i] <= var_toys[bundles[i][1] - 1]
-    prob += var_bundles[i] <= var_toys[bundles[i][2] - 1]
+    exp = var_toys[i]
+    for j in range(p):
+        if (i + 1) in bundles[j]:
+            exp += var_bundles[j]
+    prob += exp <= toys[i][1]
 
 # Restrição do número máximo de peças
-prob += lpSum(var_toys[i] for i in range(t)) + 3 * lpSum( var_bundles[i] for i in range(p)) <= m
+prob += lpSum(var_toys[i] for i in range(t)) + lpSum(3 * var_bundles[i] for i in range(p)) <= m
 
 prob.solve()
-
 print(int(value(prob.objective)))
-
