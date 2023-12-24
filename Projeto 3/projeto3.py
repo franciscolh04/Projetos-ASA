dc8ba558205a7d2032d99cc38cd66dee43d3d025
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2023-12-24 11:15:33 +0000

    Uploaded optimized version

--- Projeto 3/projeto3.py
+++ Projeto 3/projeto3.py
@@ -3,24 +3,20 @@ from pulp import *
 prob = LpProblem("Base", LpMaximize)
 
 # Recolhe informações iniciais
-toys, bundles, var_toys, var_bundles = [], [], [], []
+toys, bundles, packs_index, var_toys, var_bundles = [], [], [], [], []
 t, p, m = [int(i) for i in input().split()]
 
-# Recolhe brinquedos e respetiva capacidade
+# Recolhe brinquedos e respetiva capacidade e cria variáveis de decisão
 for i in range(t):
     toys.append([int(i) for i in input().split()])
+    packs_index.append([])
+    var_toys.append(LpVariable("x" + str(i + 1), 0, None, cat=LpInteger))
 
-# Recolhe pacotes especiais
+# Recolhe pacotes especiais e cria variáveis de decisão
 for i in range(p):
     bundles.append([int(i) for i in input().split()])
-
-# Cria variáveis de decisão: brinquedos e pacotes
-for i in range(t):
-    temp = LpVariable("x" + str(i + 1), 0, None, cat=LpInteger)
-    var_toys.append(temp)
-for i in range(p):
-    temp = LpVariable("y" + str(i + 1), 0, None, cat=LpInteger)
-    var_bundles.append(temp)
+    [packs_index[bundles[i][j] - 1].append(i) for j in range(3)]
+    var_bundles.append(LpVariable("y" + str(i + 1), 0, None, cat=LpInteger))
 
 # Função Objetivo
 prob += lpSum(var_toys[i] * toys[i][0] for i in range(t)) + lpSum(var_bundles[i] * bundles[i][-1] for i in range(p))
@@ -28,13 +24,12 @@ prob += lpSum(var_toys[i] * toys[i][0] for i in range(t)) + lpSum(var_bundles[i]
 # Restrição dos brinquedos e pacotes
 for i in range(t):
     exp = var_toys[i]
-    for j in range(p):
-        if (i + 1) in bundles[j]:
-            exp += var_bundles[j]
+    for j in range(len(packs_index[i])):
+        exp += var_bundles[packs_index[i][j]]
     prob += exp <= toys[i][1]
 
 # Restrição do número máximo de peças
 prob += lpSum(var_toys[i] for i in range(t)) + lpSum(3 * var_bundles[i] for i in range(p)) <= m
 
-prob.solve()
+prob.solve(PULP_CBC_CMD(msg=False))
 print(int(value(prob.objective)))
