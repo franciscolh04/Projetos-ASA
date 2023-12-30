029b72a265bb30773710cb42ae90768ee000beb3
Francisco Lourenço Heleno <francisco.l.heleno@tecnico.ulisboa.pt>
2023-12-30 15:42:29 +0000

    Fully working and passing all private tests (17 on Mooshak) - probably final version

--- Projeto 3/projeto3.py
+++ Projeto 3/projeto3.py
@@ -3,33 +3,36 @@ from pulp import *
 prob = LpProblem("Base", LpMaximize)
 
 # Recolhe informações iniciais
-toys, bundles, packs_index, var_toys, var_bundles = [], [], [], [], []
+toys, exps = [], []
 t, p, m = [int(i) for i in input().split()]
+lpSum_obj, lpSum_max = 0, 0
 
 # Recolhe brinquedos e respetiva capacidade e cria variáveis de decisão
 for i in range(t):
     toys.append([int(i) for i in input().split()])
-    packs_index.append([])
-    var_toys.append(LpVariable("x" + str(i + 1), 0, None, cat=LpInteger))
+    var = LpVariable("x" + str(i + 1), 0, toys[i][1], cat=LpInteger)
+    exps.append(var)
+    lpSum_obj += var * toys[i][0]
+    lpSum_max += var
 
 # Recolhe pacotes especiais e cria variáveis de decisão
 for i in range(p):
-    bundles.append([int(i) for i in input().split()])
-    [packs_index[bundles[i][j] - 1].append(i) for j in range(3)]
-    var_bundles.append(LpVariable("y" + str(i + 1), 0, None, cat=LpInteger))
+    pack = [int(j) for j in input().split()]
+    var = LpVariable("y" + str(i + 1), 0, None, cat=LpInteger)
+    for j in range(3):
+        exps[pack[j] - 1] += var
+    lpSum_obj += var * pack[-1]
+    lpSum_max += 3 * var
 
 # Função Objetivo
-prob += lpSum(var_toys[i] * toys[i][0] for i in range(t)) + lpSum(var_bundles[i] * bundles[i][-1] for i in range(p))
+prob += lpSum_obj
 
 # Restrição dos brinquedos e pacotes
 for i in range(t):
-    exp = var_toys[i]
-    for j in range(len(packs_index[i])):
-        exp += var_bundles[packs_index[i][j]]
-    prob += exp <= toys[i][1]
+    prob += exps[i] <= toys[i][1]
 
 # Restrição do número máximo de peças
-prob += lpSum(var_toys[i] for i in range(t)) + lpSum(3 * var_bundles[i] for i in range(p)) <= m
+prob += lpSum_max <= m
 
-prob.solve(PULP_CBC_CMD(msg=False))
+prob.solve(GLPK(msg=0))
 print(int(value(prob.objective)))
